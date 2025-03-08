b7c8cd0acbce870cd60b4135360cd6c1880558a2
Slava Kostevych <tomkat.s@gmail.com>
2025-03-08 13:39:22 +0200

    add env vars

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import folium,json,mgrs, urllib.request,os
 
 location        = os.getenv("DEFUALT_LOCATION", [49.06830461667328, 33.41544936380822])
 url             = os.getenv("JSON_URL", 'http://192.168.10.5:8082/get_map/json')
-
+host_port       = os.getenv("HOST_PORT", 5000)
+host_ip         = os.getenv("HOST_IP", '192.168.10.9')
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def fullscreen():
@@ -22,5 +25,5 @@ def fullscreen():
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    http_server = WSGIServer(('192.168.10.9', 5001), app)
+    http_server = WSGIServer((host_ip, host_port), app)
     http_server.serve_forever()
